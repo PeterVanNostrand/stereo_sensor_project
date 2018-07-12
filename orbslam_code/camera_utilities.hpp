f3c515833839d27829acc286fbcced40f3ed5ba0
Peter VanNostrand <petermvannostrand@gmail.com>
2018-07-12 17:36:07 -0400

    Adding unstaged changes from previous commit
    Adding unstage changes from previous commint entitled
    "Monocular_Camera complete, building, and debugged"

--- orbslam_code/camera_utilities.hpp
+++ orbslam_code/camera_utilities.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
-#include <string>
 
 namespace camera_utilities {
+    const double default_viewer_params[10] = {0.05,1,0.9,2,0.08,3,0,-0.7,-1.8,500};
+    const double default_orb_params[5] = {1000,1.2,8,20,7};
     class Monocular_Camera {
         public:
             //Constructor for camera with known intrinsic properties (matrix, dist coeffs, etc)
@@ -38,12 +42,75 @@ namespace camera_utilities {
                 camera.retrieve(frame, 0);
                 return frame;
             }
+            void create_orbslam_settings(std::string yaml_path, const double orb_params[5]=default_orb_params, const double viewer_params[10]=default_viewer_params){
+                std::fstream yaml(yaml_path, std::fstream::out);
+                if(!yaml.is_open()) throw std::runtime_error("UNABLE TO CREATE SETTINGS FILE");
+                //start of file contents
+                yaml << "%YAML:1.0" << std::endl;
+                yaml << std::endl;
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;
+                yaml << "# Camera Parameters for Monocular Camera" << std::endl;
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;
+                yaml << std::endl;
+                yaml << "# Camera calibration and distortion parameters (OpenCV) " << std::endl;
+                yaml << "Camera.fx: " << camera_matrix.at<double>(0,0) << std::endl;
+                yaml << "Camera.fy: " << camera_matrix.at<double>(1,1) << std::endl;
+                yaml << "Camera.cx: " << camera_matrix.at<double>(0,2) << std::endl;
+                yaml << "Camera.cy: " << camera_matrix.at<double>(1,2) << std::endl;
+                yaml << std::endl;
+                yaml << "Camera.k1: " << dist_coeffs.at<double>(0,0) << std::endl;
+                yaml << "Camera.k2: " << dist_coeffs.at<double>(0,1) << std::endl;
+                yaml << "Camera.p1: " << dist_coeffs.at<double>(0,2) << std::endl;
+                yaml << "Camera.p2: " << dist_coeffs.at<double>(0,3) << std::endl;
+                yaml << "Camera.k3: " << dist_coeffs.at<double>(0,4) << std::endl;
+                yaml << std::endl;
+                yaml << "Camera.fps: " << frame_rate << std::endl;
+                yaml << std::endl;
+                yaml << "# Color order of the images (0: BGR, 1: RGB. It is ignored if images are grayscale)" << std::endl;
+                yaml << "Camera.RGB: 0" << std::endl; //OpenCV always uses BGR
+                yaml << std::endl;
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;   
+                yaml << "# ORB Parameters" << std::endl;
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;
+                yaml << std::endl;
+                yaml << "# ORB Extractor: Number of features per image" << std::endl;
+                yaml << "ORBextractor.nFeatures: " << orb_params[0] << std::endl;
+                yaml << std::endl;
+                yaml << "# ORB Extractor: Scale factor between levels in the scale pyramid" << std::endl;
+                yaml << "ORBextractor.scaleFactor: " << orb_params[1] << std::endl;
+                yaml << std::endl;
+                yaml << "# ORB Extractor: Number of levels in the scale pyramid" << std::endl;
+                yaml << "ORBextractor.nLevels: " << orb_params[2] << std::endl;
+                yaml << std::endl;
+                yaml << "# ORB Extractor: Fast threshold" << std::endl;
+                yaml << "# Image is divided in a grid. At each cell FAST are extracted imposing a minimum response." << std::endl;
+                yaml << "# Firstly we impose iniThFAST. If no corners are detected we impose a lower value minThFAST" << std::endl;
+                yaml << "# You can lower these values if your images have low contrast" << std::endl;			
+                yaml << "ORBextractor.iniThFAST: " << orb_params[3] << std::endl;
+                yaml << "ORBextractor.minThFAST: " << orb_params[4] << std::endl;
+                yaml << std::endl;
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;	
+                yaml << "# Viewer Parameters" << std::endl;	
+                yaml << "#--------------------------------------------------------------------------------------------" << std::endl;	
+                yaml << "Viewer.KeyFrameSize: " << viewer_params[0] << std::endl;
+                yaml << "Viewer.KeyFrameLineWidth: " << viewer_params[1] << std::endl;
+                yaml << "Viewer.GraphLineWidth: " << viewer_params[2] << std::endl;
+                yaml << "Viewer.PointSize: " << viewer_params[3] << std::endl;
+                yaml << "Viewer.CameraSize: " << viewer_params[4] << std::endl;
+                yaml << "Viewer.CameraLineWidth: " << viewer_params[5] << std::endl;
+                yaml << "Viewer.ViewpointX: " << viewer_params[6] << std::endl;
+                yaml << "Viewer.ViewpointY: " << viewer_params[7] << std::endl;
+                yaml << "Viewer.ViewpointZ: " << viewer_params[8] << std::endl;
+                yaml << "Viewer.ViewpointF: " << viewer_params[9] << std::endl;
+                //end of file contents
+                yaml.close();
+            }
         private:
             cv::VideoCapture camera;
             cv::Size frame_size;
             int frame_rate;
             cv::Mat camera_matrix;
-            cv::Mat dist_coeffs;
+            cv::Mat dist_coeffs;            
             void calibrate_camera(cv::Size chessboard_dim){
                 camera.set(cv::CAP_PROP_FRAME_WIDTH, frame_size.width);
                 camera.set(cv::CAP_PROP_FRAME_HEIGHT, frame_size.height);
@@ -62,13 +129,13 @@ namespace camera_utilities {
                     int fail_count = 0;
                     cv::Mat frame_color;
                     cv::Mat frame_gray;
-                    cv::Mat frame_drawn;
                     while(fail_count < 10){
                         if(!camera.grab()){ //failed to get frame
                             ++fail_count;
                             continue;
                         }
                         camera.retrieve(frame_color, 0);
+                        cv::imshow("Camera Feed", frame_color);
                         int key = cv::waitKey(1);
                         if(key%256 == 27) //ESC was pressed, stop taking frames
                             break;
@@ -78,18 +145,16 @@ namespace camera_utilities {
                             if(!cv::findChessboardCorners(frame_gray, chessboard_dim, frame_corners, 0)) //no chesssboard was found
                                 continue; //jump to the next loop
                             cv::TermCriteria termcrit(cv::TermCriteria::COUNT | cv::TermCriteria::EPS,20,0.03);
-                            cornerSubPix(frame_gray, frame_corners, {5,5}, {-1,-1}, termcrit); //refine the location of the chessboard corners
+                            cv::cornerSubPix(frame_gray, frame_corners, {5,5}, {-1,-1}, termcrit); //refine the location of the chessboard corners
                             imgpoints.push_back(frame_corners); //save 2D points
                             objpoints.push_back(real_corners); //save 3D mapping of 2D points
-                            cv:: drawChessboardCorners(frame_drawn, chessboard_dim, frame_corners, true);
-                            cv::imshow("Camera Feed", frame_drawn);
+                            cv::drawChessboardCorners(frame_color, chessboard_dim, frame_corners, true);
+                            cv::imshow("Camera Feed", frame_color);
                             cv::waitKey(500);
                         }
                     }
-                    if(fail_count>=10){
-                        std::cout << "ERROR READING FROM CAMERA";
-                        throw "ERROR READING FROM CAMERA";
-                    }
+                    cv::destroyAllWindows();
+                    if(fail_count>=10) throw std::runtime_error( "UNABLE TO GET IMAGES FROM CAMERA" );
                 }
 
                 //Use objpoints and imgpoints to create camera_matrix and dist_coeffs
@@ -100,8 +165,7 @@ namespace camera_utilities {
             }    
             void load_camera_properties(std::string prop_path){
                 cv::FileStorage fs(prop_path, cv::FileStorage::READ);
-                if (!fs.isOpened())
-    	            throw "UNABLE TO OPEN PROPERTIES FILE";
+                if (!fs.isOpened()) throw std::runtime_error("UNABLE TO OPEN PROPERTIES FILE");
                 fs["frame_size"] >> frame_size;
                 fs["frame_rate"] >> frame_rate;
                 fs["camera_matrix"] >> camera_matrix;
@@ -113,8 +177,7 @@ namespace camera_utilities {
             }
             void save_camera_properties(std::string prop_path){
                 cv::FileStorage fs(prop_path, cv::FileStorage::WRITE);
-                if (!fs.isOpened())
-    	            throw "UNABLE TO CREATE PROPERTIES FILE";
+                if (!fs.isOpened())  throw std::runtime_error("UNABLE TO CREATE PROPERTIES FILE");
                 fs << "frame_size" << frame_size;
                 fs << "frame_rate" << frame_rate;
                 fs << "camera_matrix" << camera_matrix;
--- orbslam_code/orb_live.cpp
+++ orbslam_code/orb_live.cpp
@@ -1,9 +1,19 @@
-//#include "System.h"
-#include "Camera_Utilities.hpp"
+#include<opencv2/core/core.hpp>
+#include "System.h"
+#include "camera_utilities.hpp"
 
 using namespace std;
 
 int main(){
-    camera_utilities::Monocular_Camera camera(0, "./camera_properties.txt", {6,8}, {640,480}, 15);
+    camera_utilities::Monocular_Camera camera(0, "./camera_properties.txt");// {6,8}, {640,480}, 15);
+    camera.create_orbslam_settings("camera.yaml");
+    ORB_SLAM2::System SLAM("/home/peter/ORB_SLAM2/Vocabulary/ORBvoc.txt","camera.yaml",ORB_SLAM2::System::MONOCULAR,true);
+    cv::Mat frame = camera.get_frame();
+    for(unsigned long int time_stamp=0;time_stamp<500;time_stamp++){
+        SLAM.TrackMonocular(frame, time_stamp);
+        frame = camera.get_frame();
+    }
+    SLAM.Shutdown();
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
     return 0;
 }
