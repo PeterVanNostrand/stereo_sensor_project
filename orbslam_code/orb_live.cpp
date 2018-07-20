a87a5baeea705217ae10694e640889203c99a9ce
Peter VanNostrand <petermvannostrand@gmail.com>
2018-07-20 16:21:25 -0400

    Stereo SLAM running
    Changelog:
     - Completed debugging of stereo SLAM algorithm, "bad_alloc" error fom
    orbslam was caused by passing an incorrect vocabulary path (path may
       not be relative to root)
     - Edited orb_live to use vairables for paths to make for easy
       reconfiguration with a directory structure
     - orb_live now saves the frames as they are taken from the camera and
       passed to the SLAM object
     - orb_live now treats each run as a new session.  Sessions have their
       own folder in the output directory which contains the OpenCV
       properties, orbslam yaml, trajectory data, and a directory of all
       frames used
     - When loading from a previous mono configuration the previous sessions
       properties file is copied and loaded by default. If no previous
       session data is availible the user is prompted for a path to the
       opencv properties file.
    
    TODO:
     - implement loading from previous session in stereo case

--- orbslam_code/orb_live.cpp
+++ orbslam_code/orb_live.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
 #include<opencv2/core/core.hpp>
 #include "System.h"
 #include "camera_utilities.hpp"
 
 using namespace std;
+
+std::string orb_slam_dir = "/home/peter/ORB_SLAM2/";
+std::string orb_voc_path = orb_slam_dir + "Vocabulary/ORBvoc.txt";
+std::string output_dir = "../data/";
+int session_num = 0;
+string session_dir = "session_0000/";
+std::string frame_dir;
+//std::string properties_path;
+//std::string yaml_path;
+unsigned long int num_frames_to_capture = 500;
 
 void start_mono_slam(camera_utilities::Monocular_Camera mono_camera);
 void start_stereo_slam(camera_utilities::Stereo_Camera stereo_camera);
@@ -18,6 +31,19 @@ int main(){
     std::cout << "OPTION: ";
     std::cin >> option;
     
+    //check that ouput directory exits
+    if(mkdir(output_dir.c_str(), 0777) != -1) throw std::runtime_error("NO SUCH OUTPUT DIRECTORY: " + output_dir);
+    //create a new directory for this sesion
+    while(mkdir((output_dir+session_dir).c_str(), 0777) == -1){ //Failed to create folder, already exits
+			session_num++;
+			stringstream ss;
+			ss << setw(4) << setfill('0') << session_num;
+			session_dir = "session_" + ss.str() + "/";
+		}
+    //create directory to store frames in
+    frame_dir = output_dir + session_dir + "frames/";
+    mkdir(frame_dir.c_str(), 0777);
+
     if(option == '1'){
         int frame_width, frame_height, chess_width, chess_height, fps;
         std::cout << "ENTER FRAME WIDTH: ";
@@ -31,13 +57,31 @@ int main(){
         std::cout << "ENTER CHESSBOARD HEIGHT: ";
         std::cin >> chess_height;
         std::cout << "STARTING CALIBRATION..." << std::endl;
-        camera_utilities::Monocular_Camera mono_camera(0, "../data/mono_camera_properties.txt", {frame_width, frame_height}, fps, {chess_width, chess_height});
+        camera_utilities::Monocular_Camera mono_camera(0, output_dir+session_dir+"mono_camera_properties.txt", {frame_width, frame_height}, fps, {chess_width, chess_height});
         std::cout << "CALIBRATION COMPLETE. LAUNCHING ORB SLAM 2..." << std::endl << std::endl;
-        mono_camera.create_orbslam_settings("../data/mono_camera.yaml");
+        mono_camera.create_orbslam_settings(output_dir+session_dir+"mono_camera.yaml");
         start_mono_slam(mono_camera);
     }
     else if (option == '2'){
-        camera_utilities::Monocular_Camera mono_camera(0, "../data/mono_camera_properties.txt");
+        string prop_path;
+        //try to find the properties file from a previous session
+        stringstream ss;
+        ss << setw(4) << setfill('0') << (session_num-1);
+        std::string prev_session_dir = "session_" + ss.str() + "/";
+        prop_path = output_dir+prev_session_dir+"mono_camera_properties.txt";
+        std::ifstream old_properties(prop_path, std::ios::binary);
+        if(!old_properties.is_open()){ //file exists and is opened        
+            std::cout << "NO PREVIOUS SESSION. ENTER PATH TO PROPERTIES FILE: ";
+            std::cin >> prop_path;
+        }
+        old_properties.open(prop_path);
+        if(!old_properties.is_open()) throw std::runtime_error("FAILED TO OPEN PROPERTIES FILE");
+        std::ofstream new_properties(output_dir+session_dir+"mono_camera_properties.txt", std::ios::binary);
+        new_properties << old_properties.rdbuf();
+
+        //create a camera object and start the SLAM tracking
+        camera_utilities::Monocular_Camera mono_camera(0, prop_path);
+        mono_camera.create_orbslam_settings(output_dir+session_dir+"mono_camera.yaml");
         start_mono_slam(mono_camera);
     }
     else if (option == '3'){
@@ -56,13 +100,14 @@ int main(){
         std::cout << "ENTER BASELINE DISTANCE IN METERS: ";
         std::cin >>baseline;
         std::cout << "STARTING CALIBRATION..." << std::endl;
-        camera_utilities::Stereo_Camera stereo_camera(0, 2, "../data/stereo_camera_properties.txt", {frame_width,frame_height}, fps, baseline, {chess_width,chess_height});
+        camera_utilities::Stereo_Camera stereo_camera(0, 2, output_dir+session_dir+"stereo_camera_properties.txt", {frame_width,frame_height}, fps, baseline, {chess_width,chess_height});
         std::cout << "CALIBRATION COMPLETE. LAUNCHING ORB SLAM 2..." << std::endl << std::endl;
-        stereo_camera.create_orbslam_settings("../data/stereo_camera.yaml");
+        stereo_camera.create_orbslam_settings(output_dir+session_dir+"stereo_camera.yaml");
         start_stereo_slam(stereo_camera);
     }
     else if (option == '4'){
-        camera_utilities::Stereo_Camera stereo_camera(0, 2,  "../data/stereo_camera_properties.txt");
+        camera_utilities::Stereo_Camera stereo_camera(0, 2,  output_dir+session_dir+"stereo_camera_properties.txt");
+        stereo_camera.create_orbslam_settings(output_dir+session_dir+"stereo_camera.yaml");
         start_stereo_slam(stereo_camera);
     }
     else{
@@ -73,25 +118,37 @@ int main(){
 }
 
 void start_mono_slam(camera_utilities::Monocular_Camera mono_camera){
-    ORB_SLAM2::System SLAM("/home/peter/ORB_SLAM2/Vocabulary/ORBvoc.txt","/home/peter/Documents/Peter/REU/stereo_sensor_project/orbslam_code/data/mono_camera.yaml",ORB_SLAM2::System::MONOCULAR,true);
+    ORB_SLAM2::System SLAM(orb_voc_path, output_dir+session_dir+"mono_camera.yaml",ORB_SLAM2::System::MONOCULAR,true);
     cv::Mat frame;
     mono_camera.get_frame(frame);
-    for(unsigned long int time_stamp=0;time_stamp<500;time_stamp++){
+    stringstream ss;
+    int pad_to_width = to_string(num_frames_to_capture).length();
+     ss<< setw(pad_to_width) << setfill('0');
+    for(unsigned long int time_stamp=0; time_stamp<num_frames_to_capture; time_stamp++){
         SLAM.TrackMonocular(frame, time_stamp);
         mono_camera.get_frame(frame);
+        ss << setw(pad_to_width) << setfill('0') << time_stamp;
+        cv::imwrite(frame_dir+ ss.str() +".png", frame);
+        ss.str("");
     }
     SLAM.Shutdown();
-    SLAM.SaveKeyFrameTrajectoryTUM("../data/MonoKeyFrameTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM(output_dir+session_dir+"stereo_keyframe_trajectory.txt");
 }
 
 void start_stereo_slam(camera_utilities::Stereo_Camera stereo_camera){
-    ORB_SLAM2::System SLAM("~/ORB_SLAM2/Vocabulary/ORBvoc.txt","../data/stereo_camera.yaml",ORB_SLAM2::System::STEREO,true);
+    ORB_SLAM2::System SLAM(orb_voc_path, output_dir+session_dir+"stereo_camera.yaml",ORB_SLAM2::System::STEREO,true);
     cv::Mat frames[2];
     stereo_camera.get_stereo_frame_rectified(frames);
-    for(unsigned long int time_stamp=0;time_stamp<500;time_stamp++){
+    stringstream ss;
+    int pad_to_width = to_string(num_frames_to_capture).length();
+    for(unsigned long int time_stamp=0; time_stamp<num_frames_to_capture; time_stamp++){
         SLAM.TrackStereo(frames[0], frames[1], time_stamp);
         stereo_camera.get_stereo_frame_rectified(frames);
+        ss << setw(pad_to_width) << setfill('0') << time_stamp;
+        cv::imwrite(frame_dir + ss.str() +"_left.png", frames[0]);
+        cv::imwrite(frame_dir+ ss.str() +"_right.png", frames[1]);
+        ss.str("");
     }
     SLAM.Shutdown();
-    SLAM.SaveKeyFrameTrajectoryTUM("../data/StereoKeyFrameTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM(output_dir+session_dir+"stereo_keyframe_trajectory.txt");
 }
